6eb8fa8a33238178eaafae7e8d47227674643bc6
LNakandakare <lucasnakaan@gmail.com>
2020-12-04 17:54:53 -0300

    config to heroku database

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,18 +1,8 @@
-// require('dotenv').config();
 const { Pool } = require('pg');
-
 
 const dbConfig = {
   connectionString: process.env.DATABASE_URL
 }
 const database = new Pool(dbConfig);
-
-// const database = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD
-// });
 
 module.exports = database;
