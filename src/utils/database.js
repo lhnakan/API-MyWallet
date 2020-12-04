bb510b7325733166e224b9df53daf59136f40259
LNakandakare <lucasnakaan@gmail.com>
2020-12-04 17:02:40 -0300

    back deploy

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+
+// const dbConfig = {
+//   connectionString: process.env.DATABASE_URL
+// }
+// const database = new Pool(dbConfig);
+
 const database = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
