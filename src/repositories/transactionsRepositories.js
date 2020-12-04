c9e693d33f376a6cdccf8bca4605f98ea03d5284
LNakandakare <lucasnakaan@gmail.com>
2020-12-04 20:02:49 -0300

    format money string

--- src/repositories/transactionsRepositories.js
+++ src/repositories/transactionsRepositories.js
@@ -1,6 +1,7 @@
 const database = require('../utils/database');
 
 async function findUserTransactions(id) {
+    await database.query('SET LC_MONETARY TO "pt_BR.utf8"');
     try {
         const result = await database.query('SELECT * FROM transactions WHERE "userId" = $1 ORDER BY year DESC, month DESC, date DESC, id DESC', [id]);
         return result.rows;
@@ -14,6 +15,8 @@ async function create(transaction) {
     const date = parseInt(selectDate[8]+selectDate[9]);
     const month = parseInt(selectDate[5]+selectDate[6]);
     const year = parseInt('20'+selectDate[2]+selectDate[3]);
+
+    await database.query('SET LC_MONETARY TO "pt_BR.utf8"');
     
     try {
         const result = await database.query('INSERT INTO transactions (item, cost, type, "userId", date, month, year) VALUES ($1, $2,$3, $4, $5, $6, $7) RETURNING *', [transaction.item, transaction.cost, transaction.type, transaction.userId, date, month, year]);
