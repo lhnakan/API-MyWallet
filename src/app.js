3c60b943545a4c35aa6e1f2faf84e800f412d31c
LNakandakare <lucasnakaan@gmail.com>
2021-03-19 19:54:19 -0300

    refactor: connection

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -10,7 +11,6 @@ const authMiddleware = require("./middlewares/authMiddlewares");
 app.use(cors());
 app.use(express.json());
 
-
 app.post("/api/users/sign-in", usersController.postSignIn);
 app.post("/api/users/sign-up", usersController.postSignUp);
 app.post("/api/users/sign-out", authMiddleware, usersController.postSignOut);
@@ -18,11 +18,5 @@ app.post("/api/users/sign-out", authMiddleware, usersController.postSignOut);
 app.get("/api/transactions", authMiddleware, transactionsController.getTransactions);
 app.post("/api/transactions/output", authMiddleware, transactionsController.postOutput);
 app.post("/api/transactions/input", authMiddleware, transactionsController.postInput);
-
-
-
-const port = process.env.PORT;
-
-app.listen(process.env.PORT);
 
 module.exports = app;
