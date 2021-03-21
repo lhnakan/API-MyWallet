023130384c475aa2aec03c6be78bd3812637ebc4
LNakandakare <lucasnakaan@gmail.com>
2021-03-21 13:02:54 -0300

    feat: add eslint

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,53 +1,55 @@
-const transactionsSchemas = require("../schemas/transactionsSchemas");
-const transactionsRepositories = require("../repositories/transactionsRepositories");
+/* eslint-disable consistent-return */
+const transactionsSchemas = require('../schemas/transactionsSchemas');
+const transactionsRepositories = require('../repositories/transactionsRepositories');
 
-async function getTransactions(req, res) {  
+async function getTransactions (req, res) {
     const userId = req.user.id;
 
     const transactions = await transactionsRepositories.findUserTransactions(userId);
-    if(transactions.length >= 0) return res.status(200).send(transactions);    
-  
-    res.status(500).send('Erro')
+    if (transactions.length >= 0) return res.status(200).send(transactions);
+
+    res.status(500).send('Erro');
 }
 
-async function postOutput(req, res) { 
+// eslint-disable-next-line consistent-return
+async function postOutput (req, res) {
     const transactionsParams = req.body;
-    const { item, cost, selectDate } = transactionsParams
+    const { item, cost, selectDate } = transactionsParams;
 
     const { error } = transactionsSchemas.transaction.validate(transactionsParams);
     if (error) return res.status(422).send({ error: error.details[0].message });
 
     const newOutput = await transactionsRepositories.create({
-        item, 
-        cost, 
-        type: "output",
-        userId: req.user.id, 
-        selectDate
-    })
+        item,
+        cost,
+        type: 'output',
+        userId: req.user.id,
+        selectDate,
+    });
 
-    res.send(newOutput)
+    res.send(newOutput);
 }
 
-async function postInput(req, res) { 
+async function postInput (req, res) {
     const transactionsParams = req.body;
-    const { item, cost, selectDate } = transactionsParams
+    const { item, cost, selectDate } = transactionsParams;
 
     const { error } = transactionsSchemas.transaction.validate(transactionsParams);
     if (error) return res.status(422).send({ error: error.details[0].message });
 
     const newInput = await transactionsRepositories.create({
-        item, 
-        cost, 
-        type: "input",
+        item,
+        cost,
+        type: 'input',
         userId: req.user.id,
-        selectDate
-    })
+        selectDate,
+    });
 
-    res.send(newInput)
+    res.send(newInput);
 }
 
 module.exports = {
     getTransactions,
-    postOutput, 
-    postInput
-}
+    postOutput,
+    postInput,
+};
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,51 +1,49 @@
-const userSchemas = require("../schemas/userSchemas");
-const usersRepository = require("../repositories/usersRepository");
-const sessionsRepository = require("../repositories/sessionsRepository");
+const userSchemas = require('../schemas/userSchemas');
+const usersRepository = require('../repositories/usersRepository');
+const sessionsRepository = require('../repositories/sessionsRepository');
 
-async function postSignUp(req, res) {
-    const userParams = req.body; 
-    
+async function postSignUp (req, res) {
+    const userParams = req.body;
+
     const { error } = userSchemas.signUp.validate(userParams);
     if (error) {
-        return res.status(422).send({ error: error.details[0].message });    
-        
+        return res.status(422).send({ error: error.details[0].message });
     }
-    
-    const emailChecking = await usersRepository.checkEmailUnique(userParams)
-    
-    if (emailChecking) return res.status(409).json({ error: "Email is already in use" });
+
+    const emailChecking = await usersRepository.checkEmailUnique(userParams);
+
+    if (emailChecking) return res.status(409).json({ error: 'Email is already in use' });
 
     const user = await usersRepository.createNewUser(userParams);
     const userData = usersRepository.getUserData(user);
-    return res.status(201).send(userData);  
+    return res.status(201).send(userData);
 }
 
-async function postSignIn(req, res) {
+async function postSignIn (req, res) {
     const userParams = req.body;
 
     const { error } = userSchemas.signIn.validate(userParams);
-    if (error) return res.status(422).send({ error: error.details[0].message })
-    
+    if (error) return res.status(422).send({ error: error.details[0].message });
+
     const user = await usersRepository.checkEmailPassword(userParams);
-    
-    if (!user) return res.status(401).send({ error: "Wrong email or password" });
-    
+
+    if (!user) return res.status(401).send({ error: 'Wrong email or password' });
+
     const userId = user.id;
     const { token } = await sessionsRepository.createByUserId(userId);
-    
+
     const userData = usersRepository.getUserData(user, token);
-    
+
     return res.send(userData);
-};
+}
 
-async function postSignOut(req, res) {
-    const result = await sessionsRepository.deleteSession(req.user.id);  
-    res.sendStatus(result)
-};
-
+async function postSignOut (req, res) {
+    const result = await sessionsRepository.deleteSession(req.user.id);
+    res.sendStatus(result);
+}
 
 module.exports = {
     postSignUp,
     postSignIn,
-    postSignOut
+    postSignOut,
 };
